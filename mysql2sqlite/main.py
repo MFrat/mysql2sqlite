b47f2cab37a74ea1939fcb8dac900e1b7f8d450a
Max Fratane <mfratane@id.uff.br>
2018-11-25 15:41:31 -0200

    .

--- mysql2sqlite/main.py
+++ mysql2sqlite/main.py
@@ -1,7 +1,19 @@
+from argparse import ArgumentParser
+
 from context import Context
 from process.generate_dump import Process
 
-process = Process(Context.clone('dev'))
-process.exec_dump()
-print(process.dump)
+
+def define_args():
+    parser = ArgumentParser()
+    parser.add_argument('-env')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = define_args()
+    process = Process(Context.clone(args.env))
+    process.exec_dump()
+    print(process.dump)
 
